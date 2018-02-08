b7976dbaa1d1634c22a5e11c7ef76dd68184cd44
the fixer of typos <30674647+umbresp@users.noreply.github.com>
2018-02-08 08:21:00 -0600

    Update __init__.py

--- brawlstars/__init__.py
+++ brawlstars/__init__.py
@@ -4,5 +4,5 @@ from .asyncclient import *
 __author__ = 'Umbresp'
 __title__ = 'brawlstars'
 __license__ = 'MIT'
-__version__ = '0.0.9'
+__version__ = '0.1.0'
 __github__ = 'https://www.github.com/umbresp/brawlstars'
